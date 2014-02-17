5ce3b48ec9167c9a6cb5cf8d97f94f91ceb4003d
chris crowd <chriscrowd@gmail.com>
2014-02-17 20:09:16 +0000

    old data pushing to repo

--- app/assets/javascripts/head.js
+++ app/assets/javascripts/head.js
@@ -1,3 +1,5 @@
 $(document).on('ready page:load', function () {
    jQuery("#responsive_headline").fitText();
+  
+  jQuery("#responsive").fitText(1.2, { minFontSize: '10px', maxFontSize: '24px' });
 });
